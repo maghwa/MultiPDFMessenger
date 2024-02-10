92a464ceacf26dc90b39e2e8a4c83fb1e53dbc5d
Marwa EL KAMIL <87017143+maghwa@users.noreply.github.com>
2024-02-10 23:16:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import PyPDF2
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
@@ -13,13 +14,18 @@ from langchain.llms import HuggingFaceHub
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+         try:
+             pdf_reader = PdfReader(pdf)
+             for page in pdf_reader.pages:
+                 text += page.extract_text()
+         except PyPDF2.errors.PdfReadError as e:
+          st.error(f"Error reading PDF '{pdf.name}': {e}")
+
 
 
 def get_text_chunks(text):
+    if text is None:
+        return []
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -30,11 +36,14 @@ def get_text_chunks(text):
     return chunks
 
 
+
+
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = OpenAIEmbeddings(openai_api_key='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')
+    print(f"Embeddings: {embeddings}")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
 
 
 def get_conversation_chain(vectorstore):
