ac58bb0bfbbb560c0d1807c75f2bbba30aaaacdf
Marwa EL KAMIL <87017143+maghwa@users.noreply.github.com>
2024-02-10 17:14:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Multiple PDFs Messenger 📚📄 ")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
@@ -102,3 +102,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+    
+   
+   
+   
+    
